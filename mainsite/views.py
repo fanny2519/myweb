035353168aa29d26f1b294ba7da71e4c484ecf76
fanny2519 <107169686+fanny2519@users.noreply.github.com>
2022-10-08 10:26:36 +0800

    changeview
        for count, post in enumerate(posts):    # 通过循环把他们搜集到变量post_list中
            post_lists.append("No.{}:".format(str(count)) + str(post) + "<hr>")
            post_lists.append("<small>" + str(post.body.encode('utf-8')) + "</small><br><br>")

--- mainsite/views.py
+++ mainsite/views.py
@@ -7,5 +7,8 @@ def homepage(request):   # homepage函数用于获取所有文章
     posts = Post.objects.all()        # 取得所有的数据项
     post_lists = list()
     for count, post in enumerate(posts):    # 通过循环把他们搜集到变量post_list中
-        post_lists.append("No.{}:".format(str(count)) + str(post) + "<br>")
+            for count, post in enumerate(posts):    # 通过循环把他们搜集到变量post_list中
+        post_lists.append("No.{}:".format(str(count)) + str(post) + "<hr>")
+        post_lists.append("<small>" + str(post.body.encode('utf-8')) + "</small><br><br>")
+        
     return HttpResponse(post_lists) # 把变量内容输出到客户端的浏览器页面中
